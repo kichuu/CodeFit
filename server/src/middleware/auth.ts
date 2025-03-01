d8a2440526ec61bbe77ab6936d24a7af4d865a8f
karthik-k44 <karthikkarunakaran444@gmail.com>
2025-03-02 03:01:18 +0530

    added new packages

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 
 const JWT_SECRET = process.env.JWT_SECRET || "abcd"; // Replace in production
 

34f8e3de3200c578b9fce88dff850104327da05e
krishnadev <krishnadevkichus@gmail.com>
2025-03-01 23:37:59 +0530

    add bcryptjs and jsonwebtoken dependencies; set up authentication routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import candidateRoutes from "./routes/candidateRoutes";
+import authRoutes from "./routes/authRoutes";
+
 dotenv.config();
 const app = express();
 
@@ -13,6 +15,9 @@ connectDB();
 
 // Routes
 app.use("/api/candidates", candidateRoutes);
+app.use("/api/auth", authRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port http://localhost:${PORT}/`));

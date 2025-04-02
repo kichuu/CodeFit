5f59e6ac53c56da35efd16d2adf76b4f8827c341
kichuu <krishnadevr07@gmail.com>
2025-04-02 09:57:46 +0530

    feat: add logging for JWT token in middleware and format matcher array

--- client/middleware.ts
+++ client/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value; // Get JWT from cookies
-
+  console.log(token);
   if (!token) {
     return NextResponse.redirect(new URL("/", req.url)); // Redirect to home if no token
   }
@@ -12,5 +12,12 @@ export default function middleware(req: NextRequest) {
 
 // Protect only specific routes
 export const config = {
-  matcher: ["/dashboard/:path*", "/candidates/:path*", "/compare/:path*","/add-candidate/:path*","/hired/:path*","/insights/:path*"], // Add your protected routes
+  matcher: [
+    "/dashboard/:path*",
+    "/candidates/:path*",
+    "/compare/:path*",
+    "/add-candidate/:path*",
+    "/hired/:path*",
+    "/insights/:path*",
+  ], // Add your protected routes
 };
